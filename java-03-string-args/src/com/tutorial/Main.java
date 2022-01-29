4fad33d43f8a908ad31ef5b038f0bcf524d4799d
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-01-29 20:02:49 +0700

    add training String[] args

---
+++ java-03-string-args/src/com/tutorial/Main.java
@@ -0,0 +1,10 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println("hello world");
+        System.out.println("hello world " + args[0] );
+    }
+}
