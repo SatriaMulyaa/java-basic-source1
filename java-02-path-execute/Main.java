dca94822b86e16e904c13e178d869eba6749e5cb
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-01-29 17:20:45 +0700

    add training print and path execute

---
+++ java-02-path-execute/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("i read this text part I");
+    System.out.print("i read this text part II\n");
+    System.out.print("i read this text part III");
+    System.out.println(" and i read this text part IV");
+    System.out.printf("my test score is %d", 100);
+  }
+}
