45ef0cb530395e5998fd92eeab216453e2a23c9d
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-01-31 19:41:16 +0700

    operator logic

---
+++ java-11-operator-logic/src/com/tutorial/Main.java
@@ -0,0 +1,76 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        // operator logika --> operasi yang bisa kita lakukan kepada tipe data boolean
+        // OR AND XOR,  Negasi
+
+        boolean a,b,c;
+        // OR / atau (||)
+        System.out.println("==== OR (||) ====");
+        a = false;
+        b = false;
+        c = (a||b);
+        System.out.println(a + " || " + b + " = " + c);
+        a = false;
+        b = true;
+        c = (a||b);
+        System.out.println(a + " || " + b + " = " + c);
+        a = true;
+        b = false;
+        c = (a||b);
+        System.out.println(a + " || " + b + " = " + c);
+        a = true;
+        b = true;
+        c = (a||b);
+        System.out.println(a + " || " + b + " = " + c);
+
+        // AND / dan (&&)
+        System.out.println("==== AND (&&) ====");
+        a = false;
+        b = false;
+        c = (a&&b);
+        System.out.println(a + " && " + b + " = " + c);
+        a = false;
+        b = true;
+        c = (a&&b);
+        System.out.println(a + " && " + b + " = " + c);
+        a = true;
+        b = false;
+        c = (a&&b);
+        System.out.println(a + " && " + b + " = " + c);
+        a = true;
+        b = true;
+        c = (a&&b);
+        System.out.println(a + " && " + b + " = " + c);
+
+        // XOR / exlusive or (^)
+        System.out.println("==== XOR (^) ====");
+        a = false;
+        b = false;
+        c = (a^b);
+        System.out.println(a + " ^ " + b + " = " + c);
+        a = false;
+        b = true;
+        c = (a^b);
+        System.out.println(a + " ^ " + b + " = " + c);
+        a = true;
+        b = false;
+        c = (a^b);
+        System.out.println(a + " ^ " + b + " = " + c);
+        a = true;
+        b = true;
+        c = (a^b);
+        System.out.println(a + " ^ " + b + " = " + c);
+
+        // Not / Negasi  ---> flipping (!)
+        System.out.println("==== NEGASI (!) ====");
+        a = true;
+        c = !a;
+        System.out.println(a + "---> (!) = " + c);
+        a = false;
+        c = !a;
+        System.out.println(a + "---> (!) = " + c);
+
+    }
+}
