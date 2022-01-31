5ca05d275937492f864e89d09ce81d5bb732fa82
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-01-31 08:07:12 +0700

    unary operator

---
+++ java-08-unary-operator/src/com/tutorial/Main.java
@@ -0,0 +1,35 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // unary + and -
+        int number = 1;
+        System.out.printf("unary '-', %d become %d\n",number, -number);
+        System.out.printf("unary '+', %d become %d\n",number, +number);
+
+        // unary increment and decrement
+        int numbers1 = 10;
+        numbers1++;
+        numbers1++;
+        System.out.printf("unary '++', %d become %d\n",numbers1, -numbers1);
+
+        int numbers2 = 10;
+        numbers2--;
+        System.out.printf("unary '--', %d become %d\n",numbers2, -numbers2);
+
+        // post increment
+        int a = 5;
+        System.out.printf("value with '++' a post increment become = %d\n", ++a);
+        // prefix decrement
+        int b = 5;
+        System.out.printf("value with '--' a prefix increment become = %d\n", b++);
+        System.out.printf("result value of prefix increment is = %d\n", b);
+
+        // unary boolean with ! for negation
+        boolean ucup = true;
+        System.out.println("boolean value = " + ucup);
+        System.out.println("boolean value = " + !ucup);
+
+    }
+}
