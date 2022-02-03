e5fc551a8f5bb3a701b893711c643dc6712b23ea
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-02-04 06:00:08 +0700

    if else statement

---
+++ java-16-if-else-statement/src/com/tutorial/Main.java
@@ -0,0 +1,15 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("value is " + a );
+        // this is branch
+        if(a == 5){
+            System.out.println("this is true path");
+        }else{
+            System.out.println("this is false path");
+        }
+        System.out.println("selesai");
+    }
+}
