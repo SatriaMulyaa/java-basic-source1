f3f13303e47af40a012ff1ae36792afdbe118698
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-02-04 06:54:59 +0700

    if else if statement

---
+++ java-17-if-elseIF-statement/src/com/tutorial/Main.java
@@ -0,0 +1,25 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        // if else if  statement
+        int a = 5;
+        System.out.println("this is the start of program");
+
+        // expression 1
+        if(a == 5){
+            // statement1
+            System.out.println("this is action 1");
+        // expression 2
+        }else if(a == 10){
+            // statement2
+            System.out.println("this is action 2");
+        }else{
+            // statement 3
+            System.out.println("this is default action");
+        }
+        // end of the if else if statement
+        System.out.println("this is end of the program ");
+
+    }
+}
