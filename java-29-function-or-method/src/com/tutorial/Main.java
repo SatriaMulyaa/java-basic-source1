f16a00b76de52fe572f9d3b86e610380e8b9d399
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-02-11 18:58:35 +0700

    function or method

---
+++ java-29-function-or-method/src/com/tutorial/Main.java
@@ -0,0 +1,24 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        int x,y;
+        x = 10;
+        y = hitung(x);
+        System.out.println("x = " + x + " y,= " + y);
+
+        x = 20;
+        y = hitung(x);
+        System.out.println("x = " + x + " y,= " + y);
+
+        x = 40;
+        y = hitung(x);
+        System.out.println("x = " + x + " y,= " + y);
+    }
+
+    static int hitung(int input){
+        int hasil;
+        hasil = (input+2) * input;
+        return hasil;
+    }
+}
