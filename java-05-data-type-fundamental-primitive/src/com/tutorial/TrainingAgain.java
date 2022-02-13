65e7de28734230ef81061c9ade6314167ca3b880
SatriaMulyaa <satriaamulyaa@gmail.com>
2022-02-13 18:43:40 +0700

    callback memory about max min byte size data type primary

---
+++ java-05-data-type-fundamental-primitive/src/com/tutorial/TrainingAgain.java
@@ -0,0 +1,44 @@
+package com.tutorial;
+
+public class TrainingAgain {
+    public static void main(String[] args) {
+       // how to know MAX,MIN type data
+        System.out.println("===BYTE===");
+        System.out.println("MAX size data type Byte " +  Byte.MAX_VALUE + " byte");
+        System.out.println("MIN size data type Byte " +  Byte.MIN_VALUE + " byte");
+        System.out.println("MIN size data type Byte " +  Byte.BYTES + " byte");
+        System.out.println("Bit size data type Byte " + Byte.SIZE + " bit");
+        System.out.println("===SHORT===");
+        System.out.println("MAX size data type Short " +  Short.MAX_VALUE + " byte");
+        System.out.println("MIN size data type Short " +  Short.MIN_VALUE + " byte");
+        System.out.println("MIN size data type Short " +  Short.BYTES + " byte");
+        System.out.println("Bit size data type Short " + Short.SIZE + " bit");
+        System.out.println("===INT===");
+        System.out.println("MAX size data type Integer " +  Integer.MAX_VALUE + " byte");
+        System.out.println("MIN size data type Integer " +  Integer.MIN_VALUE + " byte");
+        System.out.println("MIN size data type Integer " +  Integer.BYTES + " byte");
+        System.out.println("Bit size data type Integer " + Integer.SIZE + " bit");
+        System.out.println("===LONG===");
+        System.out.println("MAX size data type Long " +  Long.MAX_VALUE + " byte");
+        System.out.println("MIN size data type Long " +  Long.MIN_VALUE + " byte");
+        System.out.println("MIN size data type Long " +  Long.BYTES + " byte");
+        System.out.println("Bit size data type Long " + Long.SIZE + " bit");
+        System.out.println("===FLOAT===");
+        System.out.println("MAX size data type Float " +  Float.MAX_VALUE + " byte");
+        System.out.println("MIN size data type Float " +  Float.MIN_VALUE + " byte");
+        System.out.println("MIN size data type Float " +  Float.BYTES + " byte");
+        System.out.println("Bit size data type Float " + Float.SIZE + " bit");
+        System.out.println("===DOUBLE===");
+        System.out.println("MAX size data type Double " +  Double.MAX_VALUE + " byte");
+        System.out.println("MIN size data type Double " +  Double.MIN_VALUE + " byte");
+        System.out.println("MIN size data type Double " +  Double.BYTES + " byte");
+        System.out.println("Bit size data type Double " + Double.SIZE + " bit");
+        System.out.println("===Character===");
+        System.out.println("type data char size = " + Character.BYTES + " byte");
+        System.out.println("type data char size = " + Character.SIZE + " bit");
+        System.out.println("===Boolean===");
+        System.out.println("type data boolean size = " + Boolean.FALSE );
+        System.out.println("type data boolean size = " + Boolean.TRUE );
+
+    }
+}
